5f0808f20b5cc80d6adc60938f0770265e913399
John Reid <johnbaronreid@netscape.net>
2020-09-28 20:55:09 +0100

    Fix fzf configuration on XPS 15

--- _fzf.zsh
+++ _fzf.zsh
@@ -1,13 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+FZF_DIR=${FZF_DIR-/usr/local/opt/fzf/}
+if [[ ! "$PATH" == *$FZF_DIR/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$FZF_DIR/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$FZF_DIR/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+source "$FZF_DIR/shell/key-bindings.zsh"
